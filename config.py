44c4d55808e57df65036926ef87d850edbccc0f3
champon1020 <nagatelu1020@gmail.com>
2021-04-23 01:16:48 +0000

    fix config

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import dataclasses
 @dataclasses.dataclass(frozen=True)
 class Config:
     epochs = 30
-    batch_size = 256
+    batch_size = 64
     base_lr = 0.1
     image_size = 112
     n_frames = 16
